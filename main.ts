e5398ac5e96277279f0f84d456176b26710dbbea
Harvey Rogers <harveyr@gmail.com>
2025-06-27 17:07:21 -0700

    wip: tags explorer

--- main.ts
+++ main.ts
@@ -74,8 +74,41 @@ export class TagComboSearchModal extends FuzzySuggestModal<TagComboListItem> {
 }
 
 export default class HarveyKitPlugin extends Plugin {
+	private cache: Map<string, string> = new Map();
+
 	async onload() {
 		// This adds an editor command that can perform some operation on the current editor instance
+
+		// TODO: update cache when file changes
+		// this.registerEvent(
+		// 	this.app.vault.on("modify", (file) => {
+		// 		// Invalidate cache when a file is modified
+		// 		if (file instanceof TFile) {
+		// 			this.fileContentCache.delete(file.path);
+		// 		}
+		// 	})
+		// );
+
+		this.addCommand({
+			id: "harvey-kit-explore-tags-spike",
+			name: "Explore tags: v2 spike",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				// const { app } = view;
+				// const { vault } = app;
+
+				const cacheKey = "tag-combos";
+				const results = this.cache.get(cacheKey);
+				if (!results) {
+					this.findAllTagCombinations()
+						.then((results) => {
+							console.log("results", results);
+							this.cache.get(cacheKey);
+						})
+						.catch(console.error);
+				}
+			},
+		});
+
 		this.addCommand({
 			id: "harvey-kit-explore-tags",
 			name: "Explore tags",
@@ -170,4 +203,27 @@ export default class HarveyKitPlugin extends Plugin {
 	}
 
 	onunload() {}
+
+	async findAllTagCombinations(): Promise<string[]> {
+		const tagRex = /(^|\s)#[a-zA-Z-/]+/gi;
+
+		const mdFiles = this.app.vault.getMarkdownFiles();
+		const results: Set<string> = new Set();
+
+		for (const file of mdFiles) {
+			const content = await this.app.vault.cachedRead(file);
+			const matches = content.match(tagRex);
+
+			if (matches) {
+				results.add(
+					matches
+						.map((match) => {
+							return match.trim();
+						})
+						.join(" ")
+				);
+			}
+		}
+		return Array.from(results);
+	}
 }
